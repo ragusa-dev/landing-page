2381364b5d3da8fbe8229e57bda344d16ce78d44
turinglabs <sebastiano.cataudo@gmail.com>
2023-07-22 16:27:41 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import rgLogo from '/logo.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={rgLogo} className="logo" />
+      </div>
+    </>
+  )
+}
+
+export default App
